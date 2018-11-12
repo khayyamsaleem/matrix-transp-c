45442c2bc49cb14224f96ec35ff692905a8aafad
Mijeong Ban <mban1@stevens.edu>
2018-11-11 21:36:26 -0500

    added codes for creating matrices and transposing them

--- transp.c
+++ transp.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main(int argc, char **argv) {
 
@@ -6,6 +8,61 @@ int main(int argc, char **argv) {
     fprintf(stderr, "Wrong number of command-line arguments. \n Usage: ./transp.c <arg1> <arg2>\n");
     return -1;
   }
+
+  int matrix_width = atoi(argv[1]);
+  int block_width = atoi(argv[2]);
+
+  /* Check if two arguments are numbers of power of 2. */
+  if((((matrix_width > 0) && !(matrix_width & (matrix_width - 1))) == 0)
+  || (((block_width > 0) && !(block_width & (block_width - 1))) == 0)) {
+    fprintf(stderr, "Error: WAIT It is not a power of 2.\n");
+    return -1;
+  }
+  if(matrix_width < block_width) {
+    fprintf(stderr, "Error: WAIT The block size cannot be greater than the matrix size.\n");
+    return -1;
+  }
+
+  float * matrix = (float *)malloc(sizeof(float) * matrix_width * matrix_width);
+
+  int i, j = 0;
+  srand(time(NULL));
+  for(i = 0; i < matrix_width; i++){
+    for(j = 0; j < matrix_width; j++){
+      // matrix[i*matrix_width + j] = ((float)rand() / (float)RAND_MAX * 100)+10;
+      matrix[i*matrix_width + j] = ((float)rand() / (float)RAND_MAX * 100.0f);
+    }
+  }
+
+  for(i = 0; i < matrix_width * matrix_width; i++){
+    printf("%f\n", matrix[i]);
+  }
+
+  printf("\n\n");
+
+  float * output_matrix = (float *)malloc(sizeof(float) * matrix_width * matrix_width);
+
+  for(i = 0; i < matrix_width; i += block_width) {
+    for(j = 0; j < matrix_width; j+= block_width) {
+      for(int k = i; k < i + block_width; ++k) {
+        for(int l = j; l < j + block_width; ++l) {
+          printf("i:%d, j:%d, k:%d, l:%d\n", i, j, k, l);
+          output_matrix[k + l*matrix_width] = matrix[l + k*matrix_width];
+          printf("matrix: %f -> output: %f\n", matrix[l + k*matrix_width], output_matrix[k + l*matrix_width]);
+        }
+      }
+    }
+  }
+
+  printf("\n\n");
+
+  i = 0;
+  for(i = 0; i < matrix_width * matrix_width; i++){
+    printf("%f\n", output_matrix[i]);
+  }
+
+  free(matrix);
+  free(output_matrix);
 
   return 0;
 }
--- transpif.c
+++ transpif.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main(int argc, char **argv) {
 
@@ -6,6 +8,55 @@ int main(int argc, char **argv) {
     fprintf(stderr, "Wrong number of command-line arguments. \n Usage: ./transpif.c <arg1> <arg2>\n");
     return -1;
   }
+
+  int matrix_height = atoi(argv[1]);
+  int matrix_width = atoi(argv[2]);
+  int block_width = atoi(argv[3]);
+
+  /* Check if the block size is a power of 2. */
+  if((((block_width > 0) && !(block_width & (block_width - 1))) == 0)) {
+    fprintf(stderr, "Error: WAIT It is not a power of 2.\n");
+    return -1;
+  }
+
+  /* Allocating memories for the matrix */
+  float * matrix = (float *)malloc(sizeof(float) * matrix_height * matrix_width);
+
+  /* Generating N x M matrix with random numbers */
+  int i, j = 0;
+  srand(time(NULL));
+  for(i = 0; i < matrix_height; i++){
+    for(j = 0; j < matrix_width; j++){
+      // matrix[i*matrix_width + j] = ((float)rand() / (float)RAND_MAX * 100)+10;
+      matrix[i*matrix_width + j] = ((float)rand() / (float)RAND_MAX * 100.0f);
+    }
+  }
+
+  for(i = 0; i < matrix_height * matrix_width; i++){
+    printf("%f\n", matrix[i]);
+  }
+
+  printf("\n\n");
+
+  float * output_matrix = (float *)malloc(sizeof(float) * matrix_height * matrix_width);
+
+  for(i = 0; i < matrix_height; i += block_width) {
+    for(j = 0; j < matrix_width; j+= block_width) {
+      for(int k = i; k < i + block_width; ++k) {
+        for(int l = j; l < j + block_width; ++l) {
+          output_matrix[k + l*matrix_height] = matrix[l + k*matrix_width];
+        }
+      }
+    }
+  }
+
+  i = 0;
+  for(i = 0; i < matrix_height * matrix_width; i++){
+    printf("%f\n", output_matrix[i]);
+  }
+
+  free(matrix);
+  free(output_matrix);
 
   return 0;
 }
