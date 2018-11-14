715318f51d5a89ccf1648a18a5bd7ad144873dbe
Mijeong Ban <mban1@stevens.edu>
2018-11-13 20:44:26 -0500

    added print statements and this code is wrong I need to fix it

--- transpif.c
+++ transpif.c
@@ -4,18 +4,23 @@
 
 int main(int argc, char **argv) {
 
+  int matrix_height = atoi(argv[1]);
+  int matrix_width = atoi(argv[2]);
+  int block_width = atoi(argv[3]);
+
   if (argc != 4) {
     fprintf(stderr, "Wrong number of command-line arguments. \n Usage: ./transpif.c <arg1> <arg2>\n");
     return -1;
   }
 
-  int matrix_height = atoi(argv[1]);
-  int matrix_width = atoi(argv[2]);
-  int block_width = atoi(argv[3]);
-
   /* Check if the block size is a power of 2. */
   if((((block_width > 0) && !(block_width & (block_width - 1))) == 0)) {
     fprintf(stderr, "Error: WAIT It is not a power of 2.\n");
+    return -1;
+  }
+
+  if(matrix_width < block_width || matrix_height < block_width) {
+    fprintf(stderr, "Error: WAIT The block size cannot be greater than the matrix size.\n");
     return -1;
   }
 
@@ -32,28 +37,59 @@ int main(int argc, char **argv) {
     }
   }
 
-  for(i = 0; i < matrix_height * matrix_width; i++){
-    printf("%f\n", matrix[i]);
+  for (i = 0; i < matrix_width; i++) {
+    for(j = 0; j < matrix_height; j++) {
+      printf("%*.*f | ", 5, 2, matrix[i * matrix_width + j]);
+    }
+    printf("\n");
   }
-
-  printf("\n\n");
+  printf("\n");
 
   float * output_matrix = (float *)malloc(sizeof(float) * matrix_height * matrix_width);
 
+  // for(i = 0; i < matrix_height; i += block_width) {
+  //   for(j = 0; j < matrix_width; j+= block_width) {
+  //     // if((i+block <= height) && (j + block <= width)) {}
+  //     for(int k = i; k < i + block_width; ++k) {
+  //       for(int l = j; l < j + block_width; ++l) {
+  //         output_matrix[k + l*matrix_height] = matrix[l + k*matrix_width];
+  //       }
+  //     }/* else {
+  //       for(int k = i; k < height; ++k) {
+  //        for (int l = j; l < width; ++l) {
+  //        output_matrix[k + l*matrix_height] = matrix[l + k*matrix_width];
+  //      }
+  //     }
+  //     */
+  //     }
+  //   }
+  // }
+
   for(i = 0; i < matrix_height; i += block_width) {
-    for(j = 0; j < matrix_width; j+= block_width) {
-      for(int k = i; k < i + block_width; ++k) {
-        for(int l = j; l < j + block_width; ++l) {
-          output_matrix[k + l*matrix_height] = matrix[l + k*matrix_width];
+    for(j = 0; j < matrix_width; j += block_width) {
+      if ((i + block_width <= matrix_height) && (j + block_width <= matrix_width)) {
+        for(int k = i; k < i + block_width; ++k) {
+          for(int l = j; l < j + block_width; ++l) {
+            output_matrix[k + l*matrix_height] = matrix[l + k*matrix_width];
+          }
+        }
+      } else {
+        for(int k = i; k < matrix_height; ++k) {
+          for(int l = j; l < matrix_width; ++l) {
+            output_matrix[k + l*matrix_height] = matrix[l + k*matrix_width];
+          }
         }
       }
     }
   }
 
-  i = 0;
-  for(i = 0; i < matrix_height * matrix_width; i++){
-    printf("%f\n", output_matrix[i]);
+  for (i = 0; i < matrix_height; i++) {
+    for(j = 0; j < matrix_width; j++) {
+      printf("%*.*f | ", 5, 2, matrix[i * matrix_width + j]);
+    }
+    printf("\n");
   }
+  printf("\n");
 
   free(matrix);
   free(output_matrix);
